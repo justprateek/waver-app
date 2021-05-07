53050b8c34f25559127335a25eb4b3481f4a0c24
Prateek Mishra <justwriteprateek@gmail.com>
2021-05-07 09:19:34 +0530

    added class

---
+++ src/temp.js
@@ -0,0 +1,15 @@
+// class person {
+//     constructor(name='Anonymous',age=0) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     getdiscription() {
+//         return `${this.name} is of age ${this.age}`
+//     }
+// }
+// const p = new person('prateek', 20);
+// console.log(p.getdiscription());
+
+
+
