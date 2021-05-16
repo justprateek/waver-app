0eaa41fc0dbba4dd8d0d72acfeb5df6f71bb914a
Prateek Mishra <justwriteprateek@gmail.com>
2021-05-16 08:22:14 +0530

    updated scss

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -64,8 +64,10 @@ export default class IndecisionApp extends React.Component {
     const subtitle = 'Put your life in the hands of a computer';
 
     return (
+      
       <div>
         <Header subtitle={subtitle} />
+        <div className="container">
         <Action
           hasOptions={this.state.options.length > 0}
           handlePick={this.handlePick}
@@ -82,6 +84,8 @@ export default class IndecisionApp extends React.Component {
           selectedOption={this.state.selectedOption}
           handleClearSelectedOption={this.handleClearSelectedOption}
         />
+        </div>
+        
       </div>
     );
   }
