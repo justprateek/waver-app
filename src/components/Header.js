1423983c4419a70ef0232166b8517915f7b3d613
Prateek Mishra <justwriteprateek@gmail.com>
2021-05-17 08:44:55 +0530

    added more scss

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,8 @@ const Header = (props) => (
     <div className="container">
       <h1 className="header__title">{props.title}</h1>
       {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
-    
-
     </div>
- </div>
+  </div>
 );
 
 Header.defaultProps = {
--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -64,28 +64,28 @@ export default class IndecisionApp extends React.Component {
     const subtitle = 'Put your life in the hands of a computer';
 
     return (
-      
       <div>
         <Header subtitle={subtitle} />
         <div className="container">
-        <Action
-          hasOptions={this.state.options.length > 0}
-          handlePick={this.handlePick}
-        />
-        <Options
-          options={this.state.options}
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption}
-        />
-        <AddOption
-          handleAddOption={this.handleAddOption}
-        />
+          <Action
+            hasOptions={this.state.options.length > 0}
+            handlePick={this.handlePick}
+          />
+          <div className="widget">
+            <Options
+              options={this.state.options}
+              handleDeleteOptions={this.handleDeleteOptions}
+              handleDeleteOption={this.handleDeleteOption}
+            />
+            <AddOption
+              handleAddOption={this.handleAddOption}
+            />
+          </div>
+        </div>
         <OptionModal
           selectedOption={this.state.selectedOption}
           handleClearSelectedOption={this.handleClearSelectedOption}
         />
-        </div>
-        
       </div>
     );
   }
